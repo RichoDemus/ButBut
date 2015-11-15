ed8e5d5ecb2ef83fef2ca4aaa75e194b94b23b44
RichoDemus <richodemus@gmail.com>
2015-11-15 15:46:19 +0100

    trying to ssh to my server

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,14 @@
 echo "deploy script called"
 ls -l deploy/
+echo "doing find"
 find . -name travis-key
+echo "find done"
+echo "pinging"
+ping -c 1 richodemus.com
+echo "done ping"
+echo "doing nslookup"
+nslookup richodemus.com
+echo "done with nslookup"
 ssh -i deploy/travis-key butbut@richodemus.com:1337 touch travis-hello-world.txt
 
 exit 0
