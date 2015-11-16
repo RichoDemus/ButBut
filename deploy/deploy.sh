cdf2bd362c9718449548c73cf63192358a3e27a0
RichoDemus <richodemus@gmail.com>
2015-11-16 22:16:56 +0100

    Trying to get the fingerprint thing to work

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 echo "deploy script called"
 ls -l deploy/
 echo "SSHing..."
-ssh -i  deploy/travis-key -p 1337 butbut@richodemus.com touch travis-hello-world.txt
+ssh -oStrictHostKeyChecking=no -i deploy/travis-key -p 1337 butbut@richodemus.com touch travis-hello-world.txt
 
 exit 0
