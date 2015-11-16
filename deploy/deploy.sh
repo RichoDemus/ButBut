ef893fa850bfa2aba14f6331fd86180cdd5b47bc
RichoDemus <richodemus@gmail.com>
2015-11-16 22:04:52 +0100

    Did some changes after contacting the travis-people

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,6 @@
 echo "deploy script called"
 ls -l deploy/
-echo "doing find"
-find . -name travis-key
-echo "find done"
-echo "pinging"
-ping -c 1 richodemus.com
-echo "done ping"
-echo "doing nslookup"
-nslookup richodemus.com
-echo "done with nslookup"
-ssh -i deploy/travis-key butbut@richodemus.com:1337 touch travis-hello-world.txt
+echo "SSHing..."
+ssh -i -p 1337 deploy/travis-key butbut@richodemus.com touch travis-hello-world.txt
 
 exit 0
