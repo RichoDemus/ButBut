2dd1e8778f7d198bcbe90a96b7d5be798aa174cb
Richard Tjerngren <richard.tjerngren@netnet.com>
2015-11-26 10:54:47 +0100

    Revert "re-enabled strict host checking"
    This reverts commit 5ccf936eed6e5fd8ffe7a2694dfec428f571ac60.

--- deploy/remote_deploy.sh
+++ deploy/remote_deploy.sh
@@ -1,6 +1,6 @@
 echo "deploy script called"
 ls -l deploy/
 echo "Attempting to deploy hash ${TRAVIS_COMMIT}"
-ssh -i deploy/travis-key -p 1337 butbut@richodemus.com "source /etc/profile && python3 deploy.py ${TRAVIS_COMMIT}"
+ssh -oStrictHostKeyChecking=no -i deploy/travis-key -p 1337 butbut@richodemus.com "source /etc/profile && python3 deploy.py ${TRAVIS_COMMIT}"
 
 exit $?
